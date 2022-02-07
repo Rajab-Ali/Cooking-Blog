c84b511ac50b175a51f8ee79649fd75b3cdecca9
Rajab Ali <chrajabali810@gmail.com>
2022-02-07 09:11:24 +0500

    make it better

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser')
 const flash = require('connect-flash')
 
 const app=express()
-const port=3000
+const port=process.env.PORT || 3000
 
 require('dotenv').config()
 
